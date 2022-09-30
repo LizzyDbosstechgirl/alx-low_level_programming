4b06d9bdc682e92b4a5d7550774cb31b2a36eab1
LizzyDbosstechgirl <aladeelizabeth91@gmail.com>
2022-09-30 12:05:46 -0700

    tasks 0

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,19 +1,28 @@
 #include "main.h"
+
 /**
- * _strcat - concatenates string from other string
- * @dest: destination string
- * @src: source string
- * Return: destination string concatenated
- */
+ * _strcat - function that concatenates
+ *          two strings.
+ *
+ * @dest: pointer to destnation input
+ * @src: pointer to source input
+ *
+ * Return: pointer to resulting string @dest
+*/
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0, j;
+	int c, c2;
 
-	while (dest[i] != '\0')
-		i++;
-	for (j = 0; src[j] != '\0'; j++, i++)
-		dest[i] = src[j];
-	dest[i] = '\0';
+	c = 0;
+	/*find the size of dest array*/
+	while (dest[c])
+		c++;
+
+	/* iterate through each src array value without the null byte*/
+	for (c2 = 0; src[c2] ; c2++)
+		/*append src[c2] to dest[c] while overwritting the null byte in dest*/
+		dest[c++] = src[c2];
+
 	return (dest);
 }
