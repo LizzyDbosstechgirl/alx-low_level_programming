625a9dc1536c5d8f5d045dbe7afbbf95f033c760
LizzyDbosstechgirl <aladeelizabeth91@gmail.com>
2022-12-12 14:18:42 -0800

    tasks0

--- 0x18-dynamic_libraries/0-memset.c
+++ 0x18-dynamic_libraries/0-memset.c
@@ -1,21 +1,21 @@
 #include "main.h"
-
 /**
- * *_memset - concatenates two strings
- * @s:  pointer
- * @b: constant byte
- * @n: n bytes of memory
- * Return: s
+ * _memset - fills string with constant char b up to n bytes
+ * @s: input pointer to string
+ * @b: constant char
+ * @n: number of  bytes
+ * Return: pointer to a string
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	for (i = 0; n > 0 ; i++, n--)
+	while
+		(i < n)
+
 	{
 		s[i] = b;
+		i++;
 	}
-
 	return (s);
 }
